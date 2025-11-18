012bf17bf702ad90e2f331f495148b074f16087a
janicedtea <janicesu2019@gmail.com>
2025-11-17 20:44:01 -0500

    initialize time/player/screen

--- kitty.py
+++ kitty.py
@@ -2,3 +2,31 @@ import pygame
 
 pygame.init()
 
+#game constants
+white = (255, 255, 255)
+black = (0, 0, 0)
+gray = (128, 128, 128)
+width = 400
+height = 500
+background = white
+
+player = pygame.image.load('kittyplayer.jpg')
+fps = 60
+timer = pygame.time.Clock()
+
+#screen
+screen = pygame.display.set_mode([width, height])
+pygame.display.set_caption('caliGO')
+
+running = True
+while running == True:
+    timer.tick(fps)
+    screen.fill(background)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
+
+    pygame.display.flip()
+pygame.quit()
